0cd087a13a447be8fe7c104b433108470220b7b4
justokaycoding <59494195+justokaycoding@users.noreply.github.com>
2021-01-24 17:19:50 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-  $output = "test";
+  require_once(__DIR__ . '/php/header.php');
+  require_once(__DIR__ . '/php/footer.php');
+  require_once(__DIR__ . '/php/shortcode.php');
+
+  $Head = new Head();
+  $Foot = new Foot();
+
+  $output = $Head->get_header();
+  $output .= get_form();
+  $output .= $Foot->get_footer();
 	echo $output;
 ?>
