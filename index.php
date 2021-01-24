86af1194772469881fb5a8b62373258347d345a8
justokaycoding <59494195+justokaycoding@users.noreply.github.com>
2021-01-24 17:24:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   $Head = new Head();
   $Foot = new Foot();
 
-  $output = $Head->get_header();
+  $output  = $Head->get_header();
   $output .= get_form();
   $output .= $Foot->get_footer();
 	echo $output;
