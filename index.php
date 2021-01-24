06d34371fe8cf14af054c896d45eea9e0388e690
justokaycoding <59494195+justokaycoding@users.noreply.github.com>
2021-01-24 16:36:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-  echo "test";
+	require_once('../Rapid_Idea/php/header.php');
+	require_once('../Rapid_Idea/php/footer.php');
+	require_once('../Rapid_Idea/php/shortcode.php');
+
+	$Head = new Head();
+	$Foot = new Foot();
+
+	$output = $Head->get_header();
+	$output .= get_form();
+	$output .= $Foot->get_footer();
+	echo $output;
 ?>
