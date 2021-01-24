818b26d842a5b7268420f4fd91f71dd6dda48a8c
justokaycoding <59494195+justokaycoding@users.noreply.github.com>
2021-01-24 17:05:39 -0600

    s
    s

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php
-  require_once(__DIR__ . '/header.php');
-  require_once(__DIR__ . '/footer.php');
-  require_once(__DIR__ . '/shortcode.php');
-
-
-  $t = __DIR__ . '/shortcode.php';
-
-  echo '<pre>';
-  var_dump($t);
-  echo '</pre>';
-
+  require_once(__DIR__ . '/php/header.php');
+  require_once(__DIR__ . '/php/footer.php');
+  require_once(__DIR__ . '/php/shortcode.php');
 
   $Head = new Head();
   $Foot = new Foot();
