b7bfeb89289d9d7d6667561359985966882b532c
justokaycoding <59494195+justokaycoding@users.noreply.github.com>
2021-02-03 17:49:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
-  require_once(__DIR__ . '/php/header.php');
-  require_once(__DIR__ . '/php/footer.php');
-  require_once(__DIR__ . '/php/shortcode.php');
+//  require_once(__DIR__ . '/php/header.php');
+//  require_once(__DIR__ . '/php/footer.php');
+//  require_once(__DIR__ . '/php/shortcode.php');
 
   //global $user_array;
 
-  $Head = new Head();
-  $Foot = new Foot();
+//  $Head = new Head();
+//  $Foot = new Foot();
 
-  $output  = $Head->get_header();
-  $output .= mark_up_generator(get_form());
-  $output .= $Foot->get_footer();
+//  $output  = $Head->get_header();
+//  $output .= mark_up_generator(get_form());
+//  $output .= $Foot->get_footer();
 	echo $output;
 ?>
