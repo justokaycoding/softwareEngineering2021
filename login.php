347a889373c7b5e9ed3e43ec4021a847e9be3d0f
hines57 <47668636+hines57@users.noreply.github.com>
2021-02-04 11:22:59 -0600

    Update login.php

--- login.php
+++ login.php
@@ -5,16 +5,18 @@ require_once(__DIR__ . '/php/shortcode.php');
 
 global $user_array;
 
-  $Head = new Head();
-  $Foot = new Foot();
+  // $Head = new Head();
+  // $Foot = new Foot();
+  //
+  // $width = array("width-6", "width-6");
+  // $type = array("code", "text");
+  //
+  // $output  = $Head->get_header();
+  // $output .=  markup($width, $type, login_form());
+  // $output .= $Foot->get_footer();
+  // $output .= '<a href="index.php">sdf</>';
+  // echo $output;
 
-  $width = array("width-6", "width-6");
-  $type = array("code", "text");
-
-  $output  = $Head->get_header();
-  $output .=  markup($width, $type, login_form());
-  $output .= $Foot->get_footer();
-  $output .= '<a href="index.php">sdf</>';
-  echo $output;
+    echo "login";
 
   ?>
