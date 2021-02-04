699174a9ec7f76848bcce31a7a0c842411459e91
hines57 <47668636+hines57@users.noreply.github.com>
2021-02-04 11:28:34 -0600

    Update home.php

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php
-  require_once(__DIR__ . '/php/header.php');
-  require_once(__DIR__ . '/php/footer.php');
-  require_once(__DIR__ . '/php/shortcode.php');
+  // require_once(__DIR__ . '/php/header.php');
+  // require_once(__DIR__ . '/php/footer.php');
+  // require_once(__DIR__ . '/php/shortcode.php');
 
   echo 'home';
 
